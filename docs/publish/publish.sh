0df49960f5bc20c5ac887157dcf1f9c610efa28e
c332030 <c332030@gmail.com>
2021-07-21 23:40:14 +0800

    feat: publish

--- docs/publish/publish.sh
+++ docs/publish/publish.sh
@@ -8,32 +8,18 @@ git_url="$1"
 branch="$2"
 
 # shellcheck disable=SC2154
-echo "git_url: ${url}, branch: ${branch}"
+echo "git_url: ${git_url}, branch: ${branch}"
 
 work_dir="/tmp/gradle/$(date +%N)"
 echo "work_dir: ${work_dir}"
 
 mkdir -p "${work_dir}"
+cd "${work_dir}"
 
-git clone --depth=1 "${git_url}" -b "${branch}" "${work_dir}"
+git clone --depth=1 "${git_url}" -b "${branch}" .
 
-image=adoptopenjdk/openjdk11-openj9:alpine
+chmod +x gradlew
+./gradlew publish
 
-docker pull "${image}"
-docker image prune -f
-
-docker run --rm \
-  \
-  --privileged=true \
-  \
-  -v /etc/localtime:/etc/localtime \
-  \
-  -v /home:/root \
-  -v "${work_dir}":/gradle \
-  \
-  -w /gradle \
-  \
-  "${image}" \
-  chmod +x gradlew && ./gradlew publish
-
+cd ..
 rm -rf "${work_dir}"
