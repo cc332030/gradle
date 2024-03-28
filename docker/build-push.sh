22d296b77c6beed7ead60bce2ec2e49490ce0c14
c332030 <c332030@c332030.com>
2024-03-28 19:10:26 +0800

    feat: dockerfile

--- docker/build-push.sh
+++ docker/build-push.sh
@@ -27,16 +27,24 @@ IMAGE=${REGISTRY}/${AUTHOR}/${PROJECT}
 IMAGE_WITH_TAG=${IMAGE}:latest
 IMAGE_BUILD_CACHE=${IMAGE}:buildcache
 
+if [ "${JAVA_VERSION}" ]; then
+  BUILD_ARGS="${BUILD_ARGS} --build-arg JAVA_VERSION=${JAVA_VERSION}"
+fi
+if [ "${BUILD_COMMAND}" ]; then
+  BUILD_ARGS="${BUILD_ARGS} --build-arg BUILD_COMMAND=\"${BUILD_COMMAND}\""
+fi
+if [ "${BOOT_JAR_PATH}" ]; then
+  BUILD_ARGS="${BUILD_ARGS} --build-arg BOOT_JAR_PATH=\"${BOOT_JAR_PATH}\""
+fi
+
 curl -sLO https://github.com/cc332030/gradle/raw/master/docker/dockerfile
 
 docker login -u="${AUTHOR}" -p="${DOCKER_TOKEN}"
 
-docker buildx rm multiarch || true
-docker buildx create --name multiarch --driver docker-container --use
+docker buildx rm docker-container || true
+docker buildx create --name docker-container --driver docker-container --use
 docker buildx build \
-  --build-arg JAVA_VERSION="${JAVA_VERSION}" \
-  --build-arg BOOT_COMMAND="${BOOT_COMMAND}" \
-  --build-arg BOOT_JAR_PATH="${BOOT_JAR_PATH}" \
+  ${BUILD_ARGS} \
   --cache-from type=registry,ref="${IMAGE_BUILD_CACHE}" \
   --cache-to type=registry,ref="${IMAGE_BUILD_CACHE}",mode=max \
   --tag "${IMAGE_WITH_TAG}" \
