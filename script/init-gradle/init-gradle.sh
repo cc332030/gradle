baa2ae4cc323d4f58aaab2dd2e2a160b3f2ddf8e
c332030 <c332030@c332030.com>
2026-01-17 20:58:13 +0800

    feat: init gradle

--- script/init-gradle/init-gradle.sh
+++ script/init-gradle/init-gradle.sh
@@ -16,6 +16,75 @@ GPG_HOME=~/.gnupg
 
 echo "
 
+fun getConfigValue(key: String): String? {
+
+    val value = System.getProperty(key)
+    if(!value.isNullOrEmpty()) {
+        return value
+    }
+    return System.getenv(key)
+}
+
+// before settings.gradle.kts
+beforeSettings {
+
+    dependencyResolutionManagement {
+
+        repositories {
+
+            val mavenCentralUrl = getConfigValue("MAVEN_CENTRAL")
+            val nexusUsername = getConfigValue("NEXUS_USERNAME")
+            val nexusPassword = getConfigValue("NEXUS_PASSWORD")
+            val nexusSnapshotUrl = getConfigValue("NEXUS_SNAPSHOT_URL")
+            val nexusReleaseUrl = getConfigValue("NEXUS_RELEASE_URL")
+
+            mavenLocal()
+
+            if (!mavenCentralUrl.isNullOrEmpty()) {
+                println("apply mavenCentralUrl: $mavenCentralUrl")
+                maven {
+                    url = uri(mavenCentralUrl)
+                }
+            }
+
+            if (!nexusSnapshotUrl.isNullOrEmpty()) {
+                println("apply nexusSnapshotUrl: $nexusSnapshotUrl")
+                maven {
+                    url = uri(nexusSnapshotUrl)
+                    credentials {
+                        username = nexusUsername
+                        password = nexusPassword
+                    }
+                }
+            }
+
+            if (!nexusReleaseUrl.isNullOrEmpty()) {
+                println("apply nexusReleaseUrl: $nexusReleaseUrl")
+                maven {
+                    url = uri(nexusReleaseUrl)
+                    credentials {
+                        username = nexusUsername
+                        password = nexusPassword
+                    }
+                }
+            }
+
+            mavenCentral()
+            gradlePluginPortal()
+
+        }
+    }
+}
+
+// before build.gradle.kts
+initscript {
+
+}
+
+" > "${WORK_PATH}/init.gradle.kts"
+
+echo "
+
 org.gradle.daemon=false
 
 org.gradle.java.installations.auto-detect=false
